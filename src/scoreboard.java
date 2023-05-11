36a99efd2acc2d7dc0a5437c666d506ef2ba81bf
Zakaria <zakaria.lazzouni@gmail.com>
2023-05-11 12:53:42 -0400

    Comments added to scoreboard class

--- src/scoreboard.java
+++ src/scoreboard.java
@@ -1,3 +1,16 @@
+/* CSCI362 Software Engineering
+ * Class Project - Mission: Math!
+ * scoreboard.java - This is the main class for scoreboard
+ * It keeps track of the score and the trys
+ *
+ * Produced: 4/15/2023
+ *
+ * @author Zakaria Lazzouni
+ * */
+
+/**
+ * Class to keep track of scores and number of attempts
+ */
 public class scoreboard {
 
     int score;
@@ -10,33 +23,66 @@ public class scoreboard {
             "You got 4/5. Silver Medal!",
             "You got 5/5. Gold Medal!"
     };
+
+    /**
+     * Constructor class, initialize score and trys to 0
+     */
     public scoreboard() {
 
          score = 0;
          trys = 0;
     }
+    /**
+     * Helper method to set the score variable
+     * @param score - keeps track of number of correct answers
+     */
     public void setScore(int score) {
         this.score = score;
 
     }
 
+    /**
+     * Helper method to set the trys variable
+     * @param trys - keeps track of number of attempts
+     */
     public void setTrys(int trys) {
         this.trys = trys;
     }
 
+    /**
+     * Helper function to retrieve score value
+     * @return int of variable score
+     */
     public int getScore() {
         return score;
     }
 
+    /**
+     * Helper function to retrieve trys value
+     * @return int of variable trys
+     */
     public int getTrys() {
         return trys;
     }
+
+    /**
+     * Helper function to increment score value
+     */
     public void addScore() {
          score++;
     }
+
+    /**
+     * Helper function to increment trys value
+     */
     public void addTrys() {
         trys++;
     }
+
+    /**
+     * Helper function to retrieve msg about the score card
+     * @return String with information on score
+     */
     public String getresult() {
         String msg = "Try Again!";
         if(score == 3) {
@@ -51,6 +97,11 @@ public class scoreboard {
         return "You got " + score + " / " + trys + "\n" + msg;
     }
 
+
+    /**
+     * Helper function to retrieve msg about message to display beside planets
+     * @return String with information on score
+     */
     public String getPlanetResult () {
         String msg;
         if (trys == 0) {
